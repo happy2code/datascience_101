458430667234db32b62a4c1791daf03dee0726e7
Senthilvel Samatharman <s.senthilvel@gmail.com>
2021-03-19 11:15:20 +0530

    adding missing files

--- DataScience/coins.py
+++ DataScience/coins.py
@@ -1,5 +1,5 @@
 
-from common.utilities import *
+from common import utilities
 
 def is_heads_or_tails(outcome) :
     return outcome in {'Heads', 'Tails'}
@@ -17,8 +17,12 @@ sample_space = {'Heads', 'Tails'}
 event_conditions = [is_heads_or_tails, is_neither, is_heads, is_tails]
 
 for event_condition in event_conditions:
-    prob = compute_probability(event_condition, sample_space)
+    prob = utilities.compute_probability(event_condition, sample_space)
     name = event_condition.__name__
     print(f"Probability of event arising from '{name}' is '{prob}'")
 
 weighted_sample_space = {'Heads': 4, 'Tails': 1}
+for event_condition in event_conditions:
+    prob = utilities.compute_probability(event_condition, weighted_sample_space)
+    name = event_condition.__name__
+    print(f"Weighted Probability of event arising from '{name}' is '{prob}'")
--- DataScience/common/utilities.py
+++ DataScience/common/utilities.py
@@ -1,9 +1,14 @@
-def get_matching_event(event_condition, sample_space):
-    return set([outcome for outcome in sample_space 
+def get_matching_events(event_condition, generic_sample_space):
+    return set([outcome for outcome in generic_sample_space 
                 if event_condition(outcome)])
 
-def compute_probability(event_condition, sample_space):
-    events = get_matching_event(event_condition, sample_space)
-    return len(events)/len(sample_space)
-
-    
+def compute_probability(event_condition, generic_sample_space):
+    matching_events = get_matching_events(event_condition, generic_sample_space)
+    if type(generic_sample_space) == type(set()):
+        event_size = len(matching_events)
+        sample_space_size = len(generic_sample_space)
+        return event_size/sample_space_size
+    else:
+        event_size = sum(generic_sample_space[outcome] for outcome in matching_events)
+        sample_space_size = sum(generic_sample_space.values())
+        return event_size/sample_space_size
